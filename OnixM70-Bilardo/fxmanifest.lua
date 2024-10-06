da2802821e22982004b85094f88ceb7759f633ad
memoonur70/OnixM70 <morizachmidt@gmx.de>
2024-10-06 02:57:49 +0200

    OnixM70-Bilardo
    OnixM70-Bilardo: Bilardo Oynama Scripti ve Mlo/ymap Bilardo Mekan

---
+++ OnixM70-Bilardo/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+file 'v_int_40.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/v_int_40.ytyp'
+client_script "ipl.lua"
+description "OnixM70-Bilardo."
+
+client_scripts {
+    'client.lua'
+  }
+ 
+
+
